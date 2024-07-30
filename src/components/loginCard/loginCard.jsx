0abda1843f9fec91b47f78046d9a402527274109
fikrinadzif <fikrinadzif35@gmail.com>
2024-07-30 20:48:29 +0700

    login card styling

--- src/components/loginCard/loginCard.jsx
+++ src/components/loginCard/loginCard.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 
 export default function LoginCard({setUser}) {
-    const [userInput, setUserInput] = useState("")
+  const [userInput, setUserInput] = useState("")
+
+
   return (
-    <div>
-        <input type="text" onChange={(e) => setUserInput(e.target.value)} />
-        <button type='button' onClick={() => setUser(userInput)}>Login</button>
-    </div>
+    <section className='flex flex-col items-center h-screen justify-center'>
+      <div className='w-[220px] flex flex-col gap-6'>
+        <div>
+          <label className='text-lg font-semibold' for="username">Username</label>
+          <input className='p-3 mt-3 w-full' id='username' type="text" onChange={(e) => setUserInput(e.target.value)} />
+        </div>
+        <button className='bg-teal-700 p-2 text-yellow-50 font-semibold' type='button' onClick={() => setUser(userInput)}>Login</button>
+      </div>
+    </section>
     
   )
 }
